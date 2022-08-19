282b62d25a1cf2efb23f06f45820374843eff001
suyeon woo <woosean999@gmail.com>
2022-08-19 18:48:46 +0900

    몽고디비 계정 추가

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.memodb
+client = MongoClient('mongodb://test:test@localhost',27017)
+db = client.memodb2
 
 @app.route('/')
 def home():
